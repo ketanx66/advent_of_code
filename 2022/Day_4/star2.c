5b21bbc5df40c1b712a4905833fe32b52a8d8e4e
Antoni Nowak <ketanx66@gmail.com>
2022-12-05 21:48:59 +0100

    Days 4 and 5

---
+++ 2022/Day_4/star2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+
+#define BUFFER_SIZE 256
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)<(b))?(b):(a))
+
+typedef struct RANGE {
+    unsigned int left;
+    unsigned int right;
+} RANGE;
+
+bool isRangeContained(RANGE range1, RANGE range2){
+    return ((range1.left<=range2.left && range1.right>=range2.right) ||
+            (range2.left<=range1.left && range2.right>=range1.right));
+}
+
+bool isRangeOverlaping(RANGE range1, RANGE range2){
+    return (isRangeContained(range1,range2) ||
+            (range1.right>=range2.left && range2.right>=range1.left));
+}
+
+int main(){
+    FILE *input_fp = fopen("./input.txt", "r");
+    if (input_fp == NULL){
+        exit(EXIT_FAILURE);
+    }
+    char buffer[BUFFER_SIZE];
+    char *p;
+    unsigned totalPairsContained = 0;
+    while(fgets(buffer, BUFFER_SIZE, input_fp)){
+        RANGE elf1;
+        RANGE elf2;
+        sscanf(buffer,"%d-%d,%d-%d", &elf1.left,&elf1.right,&elf2.left,&elf2.right);
+        totalPairsContained += isRangeOverlaping(elf1,elf2) ? 1 : 0;
+    }
+    printf("%u\n",totalPairsContained);
+}
---
+++ 2022/Day_5/star1.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+
+#define BUFFER_SIZE 256
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)<(b))?(b):(a))
+
+typedef struct CRATE
+{
+    struct CRATE* next;
+    char tag;
+} CRATE;
+
+CRATE NULL_CRATE_OBJ = {NULL,'_'};
+CRATE* NULL_CRATE = &NULL_CRATE_OBJ;
+ 
+typedef struct STACK {
+    CRATE* top;
+} STACK;
+
+CRATE* newCrate(char tag){
+    CRATE* crate = malloc(sizeof(CRATE));
+    crate->tag = tag;
+    crate->next = NULL_CRATE;
+    return crate;
+}
+
+void putCrate(STACK* stack, CRATE* crate){
+    CRATE* current_crate = stack->top;
+    if(stack->top == NULL_CRATE){
+        stack->top = crate;
+        return;
+    }
+    CRATE* last_crate = NULL_CRATE;
+    while(current_crate != NULL_CRATE){
+        last_crate = current_crate;
+        current_crate = current_crate->next;
+    }
+    last_crate->next = crate;
+}
+
+void pushCrate(STACK* stack, CRATE* crate){
+    crate->next=stack->top;
+    stack->top=crate;
+}
+
+CRATE* popCrate(STACK* stack){
+    CRATE* crate = stack->top;
+    stack->top = crate->next;
+    crate->next = NULL_CRATE;
+}
+
+void moveNCrates(STACK* stackFrom, STACK* stackDest, unsigned int noOfCrates){
+    for(unsigned int i=0;i<noOfCrates;i++){
+        pushCrate(stackDest,popCrate(stackFrom));
+    }
+}
+
+void printTopOfStacks(STACK* stacks,unsigned int noOfStacks){
+    char* result = malloc(sizeof(char)*(noOfStacks+1));
+    result[noOfStacks] = '\0';
+    for (unsigned int i=0; i<noOfStacks; i++){
+        result[i] = stacks[i].top->tag;
+    }
+    printf("%s\n",result);
+}
+
+int main(){
+    char buffer[BUFFER_SIZE] = {0};
+    char *p = NULL;
+    FILE *input_fp = fopen("./input.txt", "r");
+    if (input_fp == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned int string_len = 0;
+    unsigned int noOfStacks = 0;
+    STACK* stacks = NULL;
+
+    fgets(buffer, BUFFER_SIZE, input_fp);
+    p = strchr(buffer, '\n');
+    if (p) {
+        *p = '\0';
+    }
+
+    string_len = strlen(buffer);
+    noOfStacks = (string_len+1)/4;
+    stacks = malloc(sizeof(*stacks)*noOfStacks);
+    for(unsigned int i=0; i<noOfStacks; i++){
+        stacks[i].top = NULL_CRATE;
+    }
+
+    rewind(input_fp);
+
+    while(fgets(buffer, BUFFER_SIZE, input_fp)){
+        p = strchr(buffer, '\n');
+        if (p) {
+            *p = '\0';
+        }
+        if(buffer[1]=='1'){
+            break;
+        }
+        for(unsigned int i=0; i < noOfStacks; i++){
+            char crateTag = buffer[(i*4)+1];
+            if(crateTag >= 'A' && crateTag <= 'Z'){
+                CRATE* new_crate = newCrate(crateTag);
+                putCrate(&stacks[i],new_crate);
+            }
+        }
+    }
+    fgets(buffer, BUFFER_SIZE, input_fp);
+    while(fgets(buffer, BUFFER_SIZE, input_fp)){
+        p = strchr(buffer, '\n');
+        if (p) {
+            *p = '\0';
+        }
+        unsigned int howManyToMove = 0;
+        unsigned int stackFrom = 0;
+        unsigned int stackTo = 0;
+        sscanf(buffer,"move %d from %d to %d", &howManyToMove, &stackFrom, &stackTo);
+        moveNCrates(&stacks[stackFrom-1],&stacks[stackTo-1],howManyToMove);
+    }
+    printTopOfStacks(stacks,noOfStacks);
+}
